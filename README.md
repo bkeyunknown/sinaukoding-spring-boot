fa4060baf5a2a18b58c04dbbb0cfc56bc7453165
bkeyunknown <kikizan01@gmail.com>
2021-12-13 09:58:01 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# sinaukoding-spring-boot" 
---
+++ src/test/java/com/mrf/sinaikoding/perpustakaan/PerpustakaanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrf.sinaikoding.perpustakaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerpustakaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
