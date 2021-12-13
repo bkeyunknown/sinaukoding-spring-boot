9f2ecd97d1beb66671c5bca52a0750b304954b48
bkeyunknown <kikizan01@gmail.com>
2021-12-13 15:38:57 +0700

    add Entities DAO

---
+++ src/main/java/com/mrf/sinaikoding/perpustakaan/dao/BookDAO.java
@@ -0,0 +1,19 @@
+package com.mrf.sinaikoding.perpustakaan.dao;
+
+import com.mrf.sinaikoding.perpustakaan.entity.Book;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+public class BookDAO extends BaseDAO<Book>{
+    @Override
+    public List<Predicate> predicates(Book param, CriteriaBuilder builder, Root<Book> root, boolean isCount) {
+        List<Predicate> predicates = super.predicates(param, builder, root, isCount);
+
+        return predicates;
+    }
+}
