830c9862e8c612d8926f43c5b3dc0d234af55ec5
bkeyunknown <kikizan01@gmail.com>
2021-12-14 10:20:03 +0700

    init User and Loan Services

---
+++ src/main/java/com/mrf/sinaikoding/perpustakaan/controller/LoanController.java
@@ -0,0 +1,15 @@
+package com.mrf.sinaikoding.perpustakaan.controller;
+
+import com.mrf.sinaikoding.perpustakaan.service.LoanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("loans")
+public class LoanController extends BaseController {
+
+    @Autowired
+    private LoanService service;
+
+}
---
+++ src/main/java/com/mrf/sinaikoding/perpustakaan/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.mrf.sinaikoding.perpustakaan.controller;
+
+import com.mrf.sinaikoding.perpustakaan.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController extends BaseController {
+
+    @Autowired
+    private UserService service;
+
+}
