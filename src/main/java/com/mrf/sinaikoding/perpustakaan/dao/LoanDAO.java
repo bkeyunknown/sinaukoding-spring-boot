c1d4784d194f7e5345f1253e4acd68999a14adc4
bkeyunknown <kikizan01@gmail.com>
2021-12-15 15:33:24 +0700

    update LoanDAO findByDate

--- src/main/java/com/mrf/sinaikoding/perpustakaan/dao/LoanDAO.java
+++ src/main/java/com/mrf/sinaikoding/perpustakaan/dao/LoanDAO.java
@@ -3,9 +3,9 @@ package com.mrf.sinaikoding.perpustakaan.dao;
 import com.mrf.sinaikoding.perpustakaan.entity.Loan;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -16,4 +16,29 @@ public class LoanDAO extends BaseDAO<Loan>{
 
         return predicates;
     }
+
+    public List<Loan> findByDate(Loan param, Date startDate, Date endDate) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Loan> query = builder.createQuery(Loan.class);
+
+        Root<Loan> root = query.from(Loan.class);
+
+        if (param != null) {
+            if (param.getUser() != null) {
+                query.where(builder.equal(root.get("user").get("id"), param.getUser().getId()));
+            }
+        }
+
+        query.where(builder.between(root.get("loanDate"), startDate, endDate));
+        query.orderBy(builder.asc(root.get("id")));
+
+        root.fetch("book", JoinType.INNER);
+        root.fetch("user", JoinType.LEFT); // Sementara  menggunakan LEFT karena user belum ada
+
+        TypedQuery<Loan> typedQuery = entityManager.createQuery(query);
+
+        return typedQuery.getResultList();
+    }
+
 }
