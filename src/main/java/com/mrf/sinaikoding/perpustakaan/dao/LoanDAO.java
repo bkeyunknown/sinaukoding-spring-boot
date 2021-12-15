51b90db49255b9fc1c39cad2967030e92aee5d6a
bkeyunknown <kikizan01@gmail.com>
2021-12-15 15:45:41 +0700

    add param condition by status

--- src/main/java/com/mrf/sinaikoding/perpustakaan/dao/LoanDAO.java
+++ src/main/java/com/mrf/sinaikoding/perpustakaan/dao/LoanDAO.java
@@ -14,6 +14,12 @@ public class LoanDAO extends BaseDAO<Loan>{
     public List<Predicate> predicates(Loan param, CriteriaBuilder builder, Root<Loan> root, boolean isCount) {
         List<Predicate> predicates = super.predicates(param, builder, root, isCount);
 
+        if (param != null) {
+            if (param.getStatus() != null) {
+                predicates.add(builder.equal(root.get("status"), param.getStatus()));
+            }
+        }
+
         if (!isCount) {
             root.fetch("book", JoinType.INNER);
         }
