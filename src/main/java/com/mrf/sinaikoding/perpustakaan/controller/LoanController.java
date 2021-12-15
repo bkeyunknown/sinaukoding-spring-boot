0ff71d0fcca556537f450ab7533b40f258f5afec
bkeyunknown <kikizan01@gmail.com>
2021-12-15 11:08:36 +0700

    update LoanController CRUD

--- src/main/java/com/mrf/sinaikoding/perpustakaan/controller/LoanController.java
+++ src/main/java/com/mrf/sinaikoding/perpustakaan/controller/LoanController.java
@@ -1,15 +1,63 @@
 package com.mrf.sinaikoding.perpustakaan.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mrf.sinaikoding.perpustakaan.common.RestResult;
+import com.mrf.sinaikoding.perpustakaan.common.StatusCode;
+import com.mrf.sinaikoding.perpustakaan.entity.Loan;
 import com.mrf.sinaikoding.perpustakaan.service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
+@PreAuthorize("permitAll()")
 @RequestMapping("/loans")
 public class LoanController extends BaseController {
 
     @Autowired
     private LoanService service;
 
+    @GetMapping
+    public RestResult get(@RequestParam(value = "param", required = false) String param,
+                          @RequestParam(value = "offset") int offset,
+                          @RequestParam(value = "limit") int limit) throws JsonProcessingException {
+
+        Loan loan = param != null ? new ObjectMapper().readValue(param, Loan.class) : new Loan();
+
+        Long rows = service.count(loan);
+
+        return new RestResult(rows > 0 ? service.find(loan, offset, limit) : new ArrayList<>(), rows);
+    }
+
+    @PostMapping
+    public RestResult save(@RequestBody Loan entity) {
+        RestResult result = new RestResult(StatusCode.OPERATION_FAILED);
+
+        if (entity != null) {
+            result.setData(service.save(entity));
+            result.setStatus(StatusCode.SAVE_SUCCESS);
+        }
+
+        return result;
+    }
+
+    @PutMapping
+    public RestResult update(@RequestBody Loan entity) {
+        RestResult result = new RestResult(StatusCode.OPERATION_FAILED);
+
+        if (entity != null) {
+            result.setData(service.update(entity));
+            result.setStatus(service.update(entity) != null ? StatusCode.UPDATE_SUCCESS : StatusCode.UPDATE_FAILED);
+        }
+
+        return result;
+    }
+
+    @DeleteMapping(value = "{id}")
+    public RestResult delete(@PathVariable Long id) {
+        return new RestResult(service.delete(id) ? StatusCode.DELETE_SUCCESS : StatusCode.DELETE_FAILED);
+    }
 }
