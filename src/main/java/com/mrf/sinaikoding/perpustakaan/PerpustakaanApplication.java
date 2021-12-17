dcc30b5b9f77be72733ac85b00ad7ff12b878cc0
bkeyunknown <kikizan01@gmail.com>
2021-12-17 10:05:44 +0700

    update getCurrentUser

--- src/main/java/com/mrf/sinaikoding/perpustakaan/PerpustakaanApplication.java
+++ src/main/java/com/mrf/sinaikoding/perpustakaan/PerpustakaanApplication.java
@@ -1,13 +1,27 @@
 package com.mrf.sinaikoding.perpustakaan;
 
+import com.mrf.sinaikoding.perpustakaan.entity.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication
 public class PerpustakaanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerpustakaanApplication.class, args);
+	}
+
+	public static User getCurrentUser() {
+		try {
+			Object pricipal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+			if (pricipal != null && pricipal.getClass().equals(User.class)) {
+				return (User) pricipal;
+			}
+		} catch (Exception ignore) {}
+
+		return null;
 	}
 
 }
